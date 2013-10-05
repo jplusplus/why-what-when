70b0998ffa8c7d7400e1730868109e84c9d2357c
Bellon Pierre <bellon.pierre@gmail.com>
2013-10-05 12:52:05 +0200

    Added angular & started basic app.coffee inspired from okf-spending-stories

---
+++ public/js/vendor/angular/angular-strap-select.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('$strap.directives')
+
+.directive('bsSelect', function($timeout) {
+
+  var NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w\d]*)|(?:\(\s*([\$\w][\$\w\d]*)\s*,\s*([\$\w][\$\w\d]*)\s*\)))\s+in\s+(.*)$/;
+
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function postLink(scope, element, attrs, controller) {
+
+      var options = scope.$eval(attrs.bsSelect) || {};
+
+      $timeout(function() {
+        element.selectpicker(options);
+        element.next().removeClass('ng-scope');
+      });
+      
+      // If we have a controller (i.e. ngModelController) then wire it up
+      if(controller) {
+
+        var refresh = function(newValue, oldValue) {
+          if (!angular.equals(newValue, oldValue)) {
+            element.selectpicker('refresh');
+          }
+        };
+
+        // Watch for changes to the model value
+        scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+          refresh(newValue, oldValue);
+        });
+
+        // Watch for changes to the options
+        scope.$watch(attrs.bsSelect, function(newValue, oldValue) {
+          refresh(newValue, oldValue);
+        });
+
+      }
+
+    }
+
+  };
+
+});
